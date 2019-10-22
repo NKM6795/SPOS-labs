6408dc79b48198c2a9d9c57bb701166f38ed0930
NKM6795 <bodyaufml@gmail.com>
2019-10-22 19:56:56 +0300

    Added detect esc, added test function, fixed ui

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
-#include <stdio.h>
 #include <pthread.h>
-#include <unistd.h>
 #include <stdbool.h>
 #include <string.h>
+#include <curses.h>
+#include <time.h>
+#include <unistd.h>
 
+#include "demofuncs.h"
 
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 #define INIT_RESULT 1000000000
-#define INIT_STOP_RESULT '&'
+#define ESC_KEY 27
+#define INIT_STOP_RESULT 100
+#define CHAR_TO_EXIT 2
+#define CHAR_TO_SLEEP 3
+#define WAITING_FOR_USER_INPUT 4
+
 typedef int returnableType;
 
 
@@ -17,16 +24,15 @@ returnableType fResult = INIT_RESULT;
 returnableType gResult = INIT_RESULT;
 char waitStopResult = INIT_STOP_RESULT;
 
+
 returnableType FFunction(int x)
 {
-    sleep(4);
-    return 1;
+    return f_func_imul(x);
 }
 
 returnableType GFunction(int x)
 {
-    sleep(10);
-    return 0;
+    return g_func_imul(x);
 }
 
 
@@ -46,59 +52,166 @@ void* CalculateG(void* x)
 
 void* WaitStop()
 {
-    printf("The program works, enter 's' to stop the program\n");
+#ifdef WAIT_ESC
+    printw("The program works, click 'esc' to stop the program\n");
+    refresh();
+    noecho();
+    keypad(stdscr, true);
     while (true)
     {
-        waitStopResult = getchar();
-        if (waitStopResult == 's')
+        char current_char = getch();
+        if (current_char == ESC_KEY)
         {
+            waitStopResult = CHAR_TO_EXIT;
             pthread_cond_signal(&cond);
             return NULL;
         }
     }
+#else
+    while (true)
+    {
+        sleep(2);
+        waitStopResult = WAITING_FOR_USER_INPUT;
+
+        printw("The program works, click\n\t'a' to continue\n\t'b' to continue without prompt\n\t'c' to cancel\n");
+        refresh();
+        while (true)
+        {
+            char current_char = getch();
+            printw("\n");
+            refresh();
+
+            if (current_char == 'c')
+            {
+                waitStopResult = CHAR_TO_EXIT;
+                pthread_cond_signal(&cond);
+                return NULL;
+            }
+            else if (current_char == 'b')
+            {
+                waitStopResult = CHAR_TO_SLEEP;
+                pthread_cond_signal(&cond);
+                return NULL;
+            }
+            else if (current_char == 'a')
+            {
+                waitStopResult = INIT_STOP_RESULT;
+                pthread_cond_signal(&cond);
+                break;
+            }
+        }
+    }
+#endif
 }
 
+void stop_program()
+{
+    printw("Program was stopped\n");
+    if (fResult == INIT_RESULT)
+    {
+        printw("F function was not calculated\n");
+    }
+    if (gResult == INIT_RESULT)
+    {
+        printw("G function was not calculated\n");
+    }
+    refresh();
+}
+
+void wait_result()
+{
+    while (true)
+    {
+        pthread_cond_wait(&cond, &lock);
+        if (waitStopResult == WAITING_FOR_USER_INPUT)
+        {
+            continue;
+        }
+
+        if (fResult == 0 || gResult == 0)
+        {
+            printw("Result: 0\n");
+            return;
+        }
+
+        if (fResult != INIT_RESULT && gResult != INIT_RESULT)
+        {
+            returnableType result = (fResult * gResult);
+            printw("Result: %d\n", result);
+            return;
+        }
+
+        if (waitStopResult == CHAR_TO_EXIT)
+        {
+            stop_program();
+            return;
+        }
+    }
+}
 
 int main()
 {
-    printf("Please enter X\n");
-
-    int x;
-    scanf("%d", &x);
+    initscr();
 
     pthread_t threadF;
-    pthread_create(&threadF, NULL, CalculateF, &x);
-
     pthread_t threadG;
-    pthread_create(&threadG, NULL, CalculateG, &x);
-
     pthread_t threadWaitCancel;
-    pthread_create(&threadWaitCancel, NULL, WaitStop, NULL);
 
-    pthread_cond_wait(&cond, &lock);
-    if (waitStopResult == 's')
+    while (true)
     {
-        printf("Program was stopped");
-        return 0;
+        printw("\nPlease enter X (-1 to exit)\n");
+        refresh();
+
+        int x;
+        scanw("%d", &x);
+
+        if (x == -1)
+        {
+            break;
+        }
+
+        fResult = INIT_RESULT;
+        gResult = INIT_RESULT;
+        waitStopResult = INIT_STOP_RESULT;
+
+        if (pthread_create(&threadF, NULL, CalculateF, &x))
+        {
+            printw("Error creating F function\n");
+        }
+        if (pthread_create(&threadG, NULL, CalculateG, &x))
+        {
+            printw("Error creating G function\n");
+        }
+        if (pthread_create(&threadWaitCancel, NULL, WaitStop, NULL))
+        {
+            printw("Error creating WaitStop function\n");
+        }
+        refresh();
+
+        pthread_mutex_lock(&lock);
+
+        wait_result();
+        echo();
+        keypad(stdscr, false);
+        refresh();
+
+        pthread_mutex_unlock(&lock);
+
+        if (fResult == INIT_RESULT && pthread_cancel(threadF))
+        {
+            printw("Error canceling F function\n");
+        }
+        if (gResult == INIT_RESULT && pthread_cancel(threadG))
+        {
+            printw("Error canceling G function\n");
+        }
+        if (waitStopResult == INIT_STOP_RESULT && pthread_cancel(threadWaitCancel))
+        {
+            printw("Error canceling WaitStop function\n");
+        }
+        refresh();
     }
-
-    if (fResult == 0 || gResult == 0)
-    {
-        printf("Short-circuit\n");
-        printf("Program was ended, result: 0");
-        return 0;
-    }
-
-    pthread_cond_wait(&cond, &lock);
-
-    if (waitStopResult == 's')
-    {
-        printf("Program was stopped");
-        return 0;
-    }
-
-    returnableType result = (fResult * gResult);
-
-    printf("Program was ended, result: %d", result);
+    
+    endwin();
     return 0;
 }
